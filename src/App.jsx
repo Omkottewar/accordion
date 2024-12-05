ce8557fa41bc9eb8690de4e0e14b594f860a2ede
Omkottewar <omkottewar19.04@gmail.com>
2024-12-05 14:18:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Accordion from './Components/Accordion/Accordion'
+
+function App() {
+
+
+  return (
+<div className="bg-gray-200 py-10">
+  <Accordion />
+</div>
+  )
+}
+
+export default App
